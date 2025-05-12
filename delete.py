7ab18340019eb186c6ef078744f7271a64af5410
ItsYaBoiJeff264 <IsBeJeff@gmail.com>
2025-05-11 19:00:36 -0500

    added delete and requirements
    Signed-off-by: ItsYaBoiJeff264 <IsBeJeff@gmail.com>

---
+++ delete.py
@@ -0,0 +1,15 @@
+import os
+if os.path.exists("./output/frames/output.mp4"):
+    os.remove("./output/frames/output.mp4")
+if os.path.exists("./output/frames/output1.png"):
+    os.remove("./output/frames/output1.png")
+if os.path.exists("./output/frames/output2.png"):
+    os.remove("./output/frames/output2.png")
+if os.path.exists("./output/frames/output3.png"):
+    os.remove("./output/frames/output3.png")
+if os.path.exists("./assets/audio/voices/wyr1tts.mp3"):
+    os.remove("./assets/audio/voices/wyr1tts.mp3")
+if os.path.exists("./assets/audio/voices/wyr2tts.mp3"):
+    os.remove("./assets/audio/voices/wyr2tts.mp3")
+if os.path.exists("./assets/audio/voices/wyr3tts.mp3"):
+    os.remove("./assets/audio/voices/wyr3tts.mp3")
